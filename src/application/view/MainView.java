11ed6fd1cf3eb28b5c3279850247ff638d430dd0
tthuwng <tronghung199@gmail.com>
2021-10-05 23:34:33 -0500

    update folder structure

--- src/application/view/MainView.java
+++ src/application/view/MainView.java
@@ -1,27 +1,44 @@
-package application;
+package application.view;
 	
+import application.controller.TextHandler;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.WindowEvent;
 
-
-public class Main extends Application {
+public class MainView extends Application {
+	private Stage stage;
+	private Scene startScene;
+	
+	private BorderPane root;
+	private BorderPane textRoot;
+	private BorderPane calendarRoot;
+	
+	private Scene textScene;
+	private Scene calendarScene;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Schedulizer");
+		
+		
+primaryStage.setTitle("Schedulizer");
+		
 		GridPane grid = new GridPane();
-		grid.setHgap(5);
-		grid.setVgap(5);
 		grid.setPadding(new Insets(20));
 		
 		//Text Area
 		TextArea textArea = new TextArea();
+		textArea.setPrefHeight(1080);
+		textArea.setPrefWidth(1920);
 		grid.add(textArea, 0, 0);
 		
 		//Submit Button
@@ -30,34 +47,27 @@ public class Main extends Application {
 		
 		submitButton.setOnAction(action -> {
 			TextHandler runText = new TextHandler(textArea.getText());
-			String[] inputText = textArea.getText().split("\"|\\s");
-			int countThe = 0;
-			for (int i = 0; i < inputText.length; i++) {
-				if (inputText[i].equalsIgnoreCase("the")) {
-					countThe++;
-				}
-			}
-			String s = "s";
-			if (countThe <= 1) {
-				s = "";
-			}
-			String outputResult = "\"The\" appears " + countThe + " time" + s + " in the text";
-			Label message = new Label (outputResult);
+			
+			Label message = new Label ("Calendar appear here!");
 			StackPane window = new StackPane();
 			
 			Scene scene1 = new Scene(window, 400, 200);
 			window.getChildren().add(message);
 			Stage newWindow = new Stage();
-			newWindow.setTitle(outputResult);
+			newWindow.setTitle("Calendar");
 			newWindow.setScene(scene1);
 			newWindow.show();
         });
 		
-		Scene scene = new Scene(grid, 500, 200);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Scene scene = new Scene(grid, 500, 500);
+//		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		
 	}
+	
+
 	
 	public static void main(String[] args) {
 		launch(args);
---
+++ src/application/view/TextView.java
@@ -0,0 +1,10 @@
+package application.view;
+
+public class TextView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
