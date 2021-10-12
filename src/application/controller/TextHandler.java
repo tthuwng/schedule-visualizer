86b9d3db066a7146d7d37fcfb03a225305003f7c
maxsellers <63917117+maxsellers@users.noreply.github.com>
2021-10-12 11:28:51 -0500

    Comments to show which variables are which in TextHandler

--- src/application/controller/TextHandler.java
+++ src/application/controller/TextHandler.java
@@ -8,7 +8,21 @@ public class TextHandler {
 		inputText = textArea.split("\n");
 		runHandler();
 	}
-	
+	/*
+	 * schedule values
+	 * schedule [class][0] = Course 
+	 * schedule [class][1] = Title
+	 * schedule [class][2] = Credit
+	 * schedule [class][3] = Start Date
+	 * schedule [class][4] = End Date
+	 * schedule [class][5] = Weekdays1 (Sometimes Null)
+	 * schedule [class][6] = Time1 (Sometimes Null)
+	 * schedule [class][7] = Weekdays2 (Sometimes Null)
+	 * schedule [class][8] = Time2 (Sometimes Null)
+	 * schedule [class][9] = Building
+	 * schedule [class][10] = Class Room
+	 * schedule [class][11] = Instructor
+	 */
 	private void runHandler() {
 		int varCount = 0;
 		
@@ -19,6 +33,7 @@ public class TextHandler {
 		
 		int classAMT = varCount/11;
 		schedule = new String[classAMT][13];
+		
 		varCount = 12;
 		for(int c = 0; c<classAMT; c++)
 		{
