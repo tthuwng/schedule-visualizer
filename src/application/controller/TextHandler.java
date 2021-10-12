acec6eeff45c51d4fc5a5ee83d8a7e8e3282f25b
maxsellers <63917117+maxsellers@users.noreply.github.com>
2021-10-12 11:34:02 -0500

    c

--- src/application/controller/TextHandler.java
+++ src/application/controller/TextHandler.java
@@ -15,10 +15,10 @@ public class TextHandler {
 	 * schedule [class][2] = Credit
 	 * schedule [class][3] = Start Date
 	 * schedule [class][4] = End Date
-	 * schedule [class][5] = Weekdays1 (Sometimes Null)
-	 * schedule [class][6] = Time1 (Sometimes Null)
-	 * schedule [class][7] = Weekdays2 (Sometimes Null)
-	 * schedule [class][8] = Time2 (Sometimes Null)
+	 * schedule [class][5] = Weekdays1 (Sometimes Empty)
+	 * schedule [class][6] = Time1 (Sometimes Empty)
+	 * schedule [class][7] = Weekdays2 (Sometimes Empty)
+	 * schedule [class][8] = Time2 (Sometimes Empty)
 	 * schedule [class][9] = Building
 	 * schedule [class][10] = Class Room
 	 * schedule [class][11] = Instructor
