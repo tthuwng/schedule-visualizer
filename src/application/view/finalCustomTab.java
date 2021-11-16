c45dc37190f33d3feef428b2825a05d405ede256
ryan2625 <ryanfreas19@augustana.edu>
2021-11-16 17:56:59 -0600

    Update finalCustomTab.java

--- src/application/view/finalCustomTab.java
+++ src/application/view/finalCustomTab.java
@@ -1,37 +1,7 @@
 package application.view;
-import java.awt.Dimension;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
-import application.controller.TextHandler;
-import application.model.Course;
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-public class finalCustomTab {
+
+public class finalCustomTab extends Tab1 {
 	public static void main(String[] Args) {
 		
 	}
