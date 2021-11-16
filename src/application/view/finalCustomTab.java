dca38ff20a48699ed3afeeda7db5bc86e65a1051
ryan2625 <ryanfreas19@augustana.edu>
2021-11-16 10:05:05 -0600

    Update finalCustomTab.java

--- src/application/view/finalCustomTab.java
+++ src/application/view/finalCustomTab.java
@@ -1,5 +1,65 @@
 package application.view;
+import java.awt.Dimension;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
+import application.controller.TextHandler;
+import application.model.Course;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 public class finalCustomTab {
-
+	public static void main(String[] Args) {
+		
+	}
+	
+	public void resetFields() {
+/*					courseCode.clear();
+					courseTitle.clear();
+					classDays = null;
+					classDaysArray = null;
+					fullLocation = null;
+					classDaysList = null;
+					
+					
+					sun.setSelected(false);
+					mon.setSelected(false);
+					tue.setSelected(false);
+					wed.setSelected(false);
+					thu.setSelected(false);
+					fri.setSelected(false);
+					sat.setSelected(false);
+					
+					timeOption1.setValue(null);
+					timeOption2.setValue(null);
+					startTime.clear(); 
+					endTime.clear();
+					location.clear();
+					roomNumber.clear();
+					facultyName.clear();
+ */
+	}
 }
